ba89d8b0651c8149d88ff6cf9839ed8cdab0fe13
RNMancini <rebeccamancini.student@careerdevs.com>
2018-10-11 22:49:09 +0000

    Route Parameters

--- React Router/poketimes/src/App.js
+++ React Router/poketimes/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import Navbar from '_/components/Navbar'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
-import Contact from './components'
+import Contact from './components/Contact'
+import About from './components/About'
 
 class App extends Component {
     render() {
@@ -13,7 +14,7 @@ class App extends Component {
             <Route exact path='/' component={Home} />
             <Route path='/about' component={About} />
             <Route path='/contact' component={Contact} />
-            
+            <Route path="/:post_id" component={Post} />
             </div>
             </BrowserRouter>
             );
