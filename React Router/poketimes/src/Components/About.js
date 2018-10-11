61e25301be2d5a8da2be1557d9edfb35cb069eba
RNMancini <rebeccamancini.student@careerdevs.com>
2018-10-11 06:16:51 +0000

    React router testing

---
+++ React Router/poketimes/src/Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    retun (
+        <div className="container">
+        <h4 className="center">About</h4>
+        <p>gfgffj</p>
+        </div>
+        )
+}
+
+export default About 
---
+++ React Router/poketimes/src/README.md
@@ -0,0 +1,1 @@
+React router build experiment test
