1a220bc8dbc0f6d224ff28344a58d1caf9e8dc32
RNMancini <rebeccamancini.student@careerdevs.com>
2018-10-22 17:52:06 +0000

    Logger.js

---
+++ Express/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var logger = require('./logger');
+app.use(logger);
+
+app.use(express.static('public'));
+
+app.get('/blocks', function(request, response) {
+    var blocks = ['Fixed', 'Movable', 'Rotation'];
+    response.json(blocks);
+})
+
+app.listen(3000, function () {
+    console.log('Listening on 3000 \n');
+});
